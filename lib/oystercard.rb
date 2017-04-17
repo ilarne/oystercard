a7248750fc0fa290f3721119041d19f75fb83fee
bruxelles86 <w_gant@yahoo.co.uk>
2017-04-17 17:49:52 +0100

    Adds guard clause for topups beyond limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 class Oystercard
   attr_reader :balance
+  DEFAULT_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Top-up would exceed £#{DEFAULT_LIMIT} limit" if @balance + amount > DEFAULT_LIMIT
     @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,7 +14,10 @@ describe '#top_up' do
   it 'tops up balance by specified amount' do
     subject.top_up(15)
     expect(subject.balance).to eq 15
+end
 
+  it 'raises an error if top-up would push balance above £90' do
+     expect{ subject.top_up(100) }.to raise_error "Top-up would exceed £#{Oystercard::DEFAULT_LIMIT} limit"
   end
 
 end
