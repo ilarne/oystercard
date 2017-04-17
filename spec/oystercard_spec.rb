ab61a43cea56c717d313fd2327db12c36a9ca994
bruxelles86 <w_gant@yahoo.co.uk>
2017-04-17 15:08:33 +0100

    Initializing with balance of zero

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  it { is_expected.to respond_to(:balance) }
+
+  it 'initializes with a balance of zero' do
+    expect(subject.balance).to eq 0
+  end
 
 end
